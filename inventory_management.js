6ad23489475c3c3ad2775aa661379e46a6f73855
coryrmrz <coryramirez2018@gmail.com>
2024-09-15 20:16:49 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,9 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    {name: 'Blanket', price: 12.99, quantity: 37, lowStockLevel: 5}, //Array information for a blanket
+    {name: 'Pillow', price: 10.99, quantity: 83, lowStockLevel: 5}, //Array info for a pillow
+    {name: 'Mattress', price: 1499.99, quantity: 28, lowStockLevel: 5}, // Array info for a mattress
+    {name: 'Pillow Case', price: 5.99, quantity: 66, lowStockLevel: 6}, //Array info for a pillow case
+    {name: 'Sheet', price: 8.99, quantity: 29, lowStockLevel: 5}, // Array info for a sheet
+];
